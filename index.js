802b7cfc66cd37530c57c36e2ae50974b7735973
Omoruyi Ohuoba <omoruyiohuoba@Omoruyis-MacBook-Pro.local>
2023-06-16 17:45:41 +0100

    production push

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const enforce = require('express-sslify');
+const path = require('path');
 const app = express();
 const cors = require('cors')
 const bodyParser = require('body-parser');
@@ -29,11 +31,53 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api/omoruyiohuobahmadns", LoginRegisterRoute, DashboardRoute, 
+UserRoute, PatientRoute,DoctorRoute, AppointmentRoute, MedicineRoute, PrescriptionRoute,
+InvoiceRoute, ProfileRoute, DrugNotificationRoute);
 
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb+srv://omoruyiohuobahmadns:omoruyiohuobahmadns@hospitalmanagementanddr.4c4mp3o.mongodb.net/?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log("Database Connected Successfully"))
 .catch(err => console.log(err));
+
+
+/*Always put this after the app.use(s) so that get requests will work and the enforce https is to push it to https url*/
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(enforce.HTTPS({ trustProtoHeader: true }))
+    app.use(express.static('client/build'));
+    
+    app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    }); 
+  };  
+
+  app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  app.use((err, req, res) => { 
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+  
+    res.json({
+      errors: {
+        message: err.message,
+        error: {},
+      },
+    });
+  });
 
 
 app.listen(port, () => {
@@ -52,9 +96,7 @@ app.use(InvoiceRoute);
 app.use(ProfileRoute);
 */
 
-app.use("/api/omoruyiohuobahmadns", LoginRegisterRoute, DashboardRoute, 
-UserRoute, PatientRoute,DoctorRoute, AppointmentRoute, MedicineRoute, PrescriptionRoute,
-InvoiceRoute, ProfileRoute, DrugNotificationRoute);
+
 
 
 
